c30d7bf6572a005e64948cb5c5f43b634e58401f
halilibrahimbeyduz <halbeyduz@hotmail.com>
2021-10-19 00:25:42 +0300

    18 Ekim Ders Sonu

---
+++ javaScriptStarterKit-master/src/app.js
@@ -0,0 +1,20 @@
+ let a = "dolar"
+console.log(a)
+
+let b = 9.20
+console.log(b)
+
+const euroDun = 11.3
+// euroDun = 11
+console.log(euroDun)
+
+// ARRAY - DİZİ
+let konutKredileri = ["Konut","Emlak","Kamu Konut","Özel Konut"]
+
+
+for(let i=0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li")
+}
+
+
+
